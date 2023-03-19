7f4fe74d005ec9ae4721f2cad02430148f63b8b9
Sergey Okatov <svok@users.noreply.github.com>
2023-03-19 16:43:07 +0500

    Update mappers/src/main/kotlin/com/crowdproj/marketplace/product/groups/mappers/exceptions/UnknownProductGroupCommand.kt

--- mappers/src/main/kotlin/com/crowdproj/marketplace/product/groups/mappers/exceptions/UnknownProductGroupCommand.kt
+++ mappers/src/main/kotlin/com/crowdproj/marketplace/product/groups/mappers/exceptions/UnknownProductGroupCommand.kt
@@ -2,4 +2,4 @@ package com.crowdproj.marketplace.product.groups.mappers.exceptions
 
 import com.crowdproj.marketplace.product.groups.common.models.ProductGroupCommand
 
-class UnknownProductGroupCommand(command: ProductGroupCommand) : Throwable("Wrong command $command at mapping toTransport stage")
+class UnknownProductGroupCommand(command: ProductGroupCommand) : RuntimeException("Wrong command $command at mapping toTransport stage")
